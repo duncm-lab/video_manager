20686dd9043a8d387812c4816df3ab99e75d78f4
dunc <dunc@ubuntu>
2020-09-30 21:31:53 +0000

    using flask for web endpoint, easier to use and better supported

--- app/sync_video/sync_video.py
+++ app/sync_video/sync_video.py
@@ -2,52 +2,29 @@
 from __future__ import unicode_literals
 import sys
 import os
-import web
+import logging
+from flask import Flask
 
 APP_PATH = os.path.dirname(__file__)
 sys.path.insert(0, APP_PATH)
 
 from add_to_queue import add_queue
 
+logging.basicConfig(filename = '/sync_video_logs/sync_video.log',
+        level = logging.DEBUG)
 
-URLS = ('/', 'Index',
-        '/syncWL', 'SyncWatchLater',
-        '/Sync', 'SyncVideo'
-        )
+logger = logging.getLogger()
 
+app = Flask(__name__)
 
-class SyncVideo:
-    def GET(self):
-        try:
-            video_id = web.input(video_id=0)
-            add_queue(video_id.video_id)
-        except Exception as e:
-            with open('sync_exception.log', 'w') as fl:
-                fl.write(str(e))
+@app.route('/')
+def index():
+    return 'sync_video index'
 
-
-class Index:
-    def GET(self):
-        
-        return '<h1>Index</h1>'
-
-
-class SyncWatchLater:
-    """Check the watch later list and removed
-    any videos not listed there"""
-    def GET(self):
-        video_ids = web.input(video_ids=0)
-        wl_videos = video_ids.video_ids.split(',')
-        for video in wl_videos:
-            get_video(video)
-
-
-#application = web.application(URLS, globals()).wsgifunc()
-try:
-    application = web.application(URLS, globals())
-except Exception as e:
-    with open('app.log', 'w') as fl:
-        fl.write(str(e))
-
-if __name__ == '__main__':
-    application.run()
+@app.route('/Sync/<video_id>')
+def sync_video(video_id):
+    try:
+        add_queue(video_id)
+    except Exception as e:
+        logger.error(e)
+    return ''
