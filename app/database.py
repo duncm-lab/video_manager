e3600674ec365cd5e3b32d2bce74f03017fc2242
dunc <dunc@ubuntu>
2020-10-23 23:21:23 +0000

    updated .travis.yml to include mongodb

--- app/database.py
+++ app/database.py
@@ -5,5 +5,11 @@ import pymongo
 from app.config import DBConfig as cfg
 
 CLIENT = pymongo.MongoClient(cfg.mongo_server)
+
+if 'video' not in CLIENT.list_database_names():
+    newdb = CLIENT[cfg.mongo_database]
+    newdb.create_collection(cfg.mongo_collection)
+
+
 DATABASE = CLIENT[cfg.mongo_database]
 COLLECTION = DATABASE[cfg.mongo_collection]
